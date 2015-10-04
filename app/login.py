175ba9cd6575aae006a14086697a2dfe6f682dfb
Rafi Lurie <Rafi@Rafis-MacBook-Pro.local>
2015-10-04 12:00:51 -0700

    continuing fixing stupid stuff more2

--- app/login.py
+++ app/login.py
@@ -2,7 +2,6 @@ import re
 
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, validators
-from validate_email import validate_email
 from .models import User
 
 def enforce_password_requirements(password):
