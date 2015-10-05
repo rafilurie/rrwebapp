9aa1329e129a41996c9d2ee6fd7920201e932092
Rafi Lurie <Rafi@Rafis-MacBook-Pro.local>
2015-10-04 19:22:02 -0700

    adding delete-btn.js

---
+++ app/static/delete-btn.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+
+    // $('#create-dropdown').hide();
+    // $('#help-dropdown').hide();
+
+    $('.delete-btn').on("click", function(event) {
+        event.preventDefault();
+        var post_url = "/delete";
+        var deletelinkID = $(this).attr('id');
+        var linkID = deletelinkID.slice(6);
+        //alert(deletelinkID);
+        //alert(linkID);
+        //var data = { content: $("textarea#about_me").val() }
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            url: post_url,
+            data: (linkID),
+            success: function(data) {
+            },
+            dataType: "json"
+        });
+    });
+
+}
