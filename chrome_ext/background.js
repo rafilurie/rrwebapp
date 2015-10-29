15c6ecb74685e98e529b07d21ed03b6bbd222e82
Reah Miyara <reahmiyara@Reahs-MacBook-Pro.local>
2015-10-29 00:13:48 -0400

    added right click and refactored code.

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -1,106 +1,76 @@
 // var POST_ROUTE = 'http://thoughtmic.herokuapp.com/post';
 var POST_ROUTE = 'http://127.0.0.1:5000/post';
-var CURRENT_TAB;
+var CONTEXT_MENU_TITLE = "Add to ThoughtMic";
+var CHROME_ICON_WHITE = "chrome_ext/icon_invert16.png";
+var CHROME_ICON_BLACK = "chrome_ext/icon16.png";
+var FLASH = 150;
 
 // Set up context menu at install time.
 chrome.runtime.onInstalled.addListener(function() {
   var context = "selection";
-  var title = "Add to ReadMate";
-  var id = chrome.contextMenus.create({"title": title, "contexts": [context],
-                                         "id": "context" + context });  
+  var title = CONTEXT_MENU_TITLE;
+  var id = chrome.contextMenus.create({"title": title, "contexts": [context],"id": "context" + context });
 });
 
-function flashIcon() { 
-    chrome.browserAction.setIcon({path: "chrome_ext/icon_invert16.png"});
-    setTimeout(function() { 
-        chrome.browserAction.setIcon({path: "chrome_ext/icon16.png"});
-    }, 150);
-}
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-    var data = { title: tab.title, url: tab.url };
-    // construct an HTTP request
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        xhr.responseText;
-        }
-    }
-    postHelper(xhr, POST_ROUTE, data);
-    flashIcon();
-});
-
-// add click event
-chrome.contextMenus.onClicked.addListener(contextMenusOnClickHandler);
-
-// The onClicked callback function.
-function contextMenusOnClickHandler(info, tab) {
-    // alert('This url: '+info.linkUrl);
-    // var data, title;
-    // // construct an HTTP request
-    // var xhr = new XMLHttpRequest();
-    // alert('I am here');
-    // xhr.onreadystatechange = function() {
-    // alert('not I am here');
-    // if (xhr.readyState == 4) {
-    //     alert('in if');
-    //     title = getTitle(xhr.responseText);
-    //     alert('title: ' + title);
-    //     data = { title: title, url: info.linkUrl };
-    //     postHelper(xhr, POST_ROUTE, data);
-    //     flashIcon();
-    //     }
-    // }
-    // xhr.open("GET", info.linkUrl, true);
-    // xhr.send();
-
-    var data = { title: info.linkUrl, url: info.linkUrl };
-    // construct an HTTP request
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        alert(xhr.responseText);
-        xhr.responseText;
-        }
-    }
-    postHelper(xhr, POST_ROUTE, data);
-    flashIcon();
-};
-
-//Add the link of the current tab when you use the keyboard shortcut
-chrome.commands.onCommand.addListener( function(command) {
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        CURRENT_TAB = tabs[0];
-        postKeyboardShortcut(CURRENT_TAB);
-    });
-});
-
-function postKeyboardShortcut(tab) { 
-    var tabTitle = tab.title;
-    var tabURL = tab.url;
-    var data = { title: tabTitle, url: tabURL };
-    //construct an HTTP request
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        xhr.responseText;
-        }
-    }
-    postHelper(xhr, POST_ROUTE, data);
-    flashIcon();
-}
-
-
-function getTitle(str) { 
+function _getTitle(str) {
     var matches = str.match(/<title>[\S\s]*?<\/title>/gi);
     return matches[0].replace(/(<\/?[^>]+>)/gi, '');
 }
 
-function postHelper(xhr, url, data) { 
+function _flashIcon() {
+    chrome.browserAction.setIcon({path: CHROME_ICON_WHITE});
+    setTimeout(function() {
+        chrome.browserAction.setIcon({path: CHROME_ICON_BLACK});
+    }, FLASH);
+}
+
+function _postHelper(xhr, url, data) {
     xhr.open("POST", url, true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.setRequestHeader("Content-length", data.length);
-
-    // send the collected data as JSON
+    xhr.setRequestHeader('Content-length', data.length);
     xhr.send(JSON.stringify(data));
 }
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    var data = { title: tab.title, url: tab.url };
+    var xhr = new XMLHttpRequest();
+    _postHelper(xhr, POST_ROUTE, data);
+    _flashIcon();
+});
+
+function contextMenusOnClickHandler(info, tab) {
+    var title = url = info.linkUrl;
+    var data = { title: title, url: url };
+    var xhr = new XMLHttpRequest();
+
+    $.ajax({
+      url: url,
+      crossDomain: true,
+      success: function(html) {
+        data.title = _getTitle(html);
+        _postHelper(xhr, POST_ROUTE, data);
+        _flashIcon();
+      },
+      error: function(a, b, c) {
+        _postHelper(xhr, POST_ROUTE, data);
+      }
+    });
+};
+chrome.contextMenus.onClicked.addListener(contextMenusOnClickHandler);
+
+function _postKeyboardShortcut(tab) {
+    var title = tab.title;
+    var url = tab.url;
+    var data = { title: title, url: url };
+    var xhr = new XMLHttpRequest();
+    _postHelper(xhr, POST_ROUTE, data);
+    _flashIcon();
+}
+
+chrome.commands.onCommand.addListener( function(command) {
+  var currentTab;
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        currentTab = tabs[0];
+        _postKeyboardShortcut(currentTab);
+    });
+});
